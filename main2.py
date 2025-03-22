d5e14d42d9df40f1bae172df8f03eca91a788169
dimasfuu <dimasfurqon@gmail.com>
2025-03-22 22:38:19 +0700

    update

--- main2.py
+++ main2.py
@@ -6,8 +6,12 @@ import numpy as np
 
 
 # Dictionary untuk pemilihan model
-muat_model_xg = joblib.load("model_rasio_testing_20extremegradientboostingregressor.joblib")
-
+muat_model_lr20 = joblib.load("model_rasio_testing_20linearregression.joblib")
+muat_model_rr20 = joblib.load("model_rasio_testing_20ridgeregression.joblib")
+muat_model_ls20 = joblib.load("model_rasio_testing_20lassoregression.joblib")
+muat_model_dt20 = joblib.load("model_rasio_testing_20decisiontreeregression.joblib")
+muat_model_gb20 = joblib.load("model_rasio_testing_20gradientboostingregressor.joblib")
+muat_model_xg20 = joblib.load("model_rasio_testing_20extremegradientboostingregressor.joblib")
 
 # model = [muat_model_lr,muat_model_rr,muat_model_ls,muat_model_dt,muat_model_dt,muat_model_gb,muat_model_xg]
 # Memilih model berdasarkan pilihan pengguna
@@ -28,7 +32,7 @@ st.title("Persentase Kepatuhan Perawatan Pasien")
 st.write("Masukkan data pasien untuk mengetahui level kepatuhan pasien.")
 
 #pilih model
-model_option = st.selectbox("Pilih Model",("model_linear_regression.joblib","model_ridge_regression.joblib","model_lasso_regression.joblib","model_decision_tree.joblib","model_gradient_boosting.joblib","model_rasio_testing_20extremegradientboostingregressor.joblib")) # pilihan model
+model_option = st.selectbox("Pilih Model",("Model Linear Regression 20:80","Model Ridge Regression 20:80","Model Lasso Regression 20:80","Model Decision Tree Regression 20:80","Model Gradient Boosting 20:80","Model Extreme Gradient Boosting 20:80")) # pilihan model
 
 import streamlit as st
 
@@ -58,37 +62,36 @@ duration = st.number_input("Masukan durasi perawatan dalam sepekan (dalam jam):"
 stress = st.slider("Masukan skor tingkat stress (1-10) : ",min_value=1, max_value= 10, step=1)  
 # Target = st.number_input("Tingkat kepatuhan pasien adalah:", min_value=0, step=1) 
 
-if st.button("Prediksi"):  # Jika tombol ditekan
-    model = muat_model_xg
-
 # if st.button("Prediksi"):  # Jika tombol ditekan
-#     if model_option == "model_linear_regression.joblib":
-#         model = muat_model_lr
-#     elif model_option == "model_ridge_regression.joblib":
-#         model = muat_model_rr
-#     elif model_option == "model_lasso_regression.joblib":
-#         model = muat_model_ls
-#     elif model_option == "model_decision_tree.joblib":
-#         model = muat_model_dt
-#     elif model_option == "model_gradient_boosting.joblib":
-#         model = muat_model_gb
-#     else:
-#         model = muat_model_xg
+#     model = muat_model_xg
 
- # Prediksi berdasarkan input pengguna
-input_data = np.array([[Age, physical, sleep,duration, stress]])#membentuk array input
-y_output = model.predict(input_data)
+if st.button("Prediksi"):  # Jika tombol ditekan
+    if model_option == "Model Linear Regression 20:80":
+        model = muat_model_lr20
+    elif model_option == "Model Ridge Regression 20:80":
+        model = muat_model_rr20
+    elif model_option == "Model Lasso Regression 20:80":
+        model = muat_model_ls20
+    elif model_option == "Model Decision Tree Regression 20:80":
+        model = muat_model_dt20
+    elif model_option == "Model Decision Tree Regression 20:80":
+        model = muat_model_gb20
+    else:
+        model = muat_model_xg20
+    # Prediksi berdasarkan input pengguna
 
-st.sidebar.write(f"Prediksi nilai Y: {y_output[0]:.2f}")
+    input_data = np.array([[Age, physical, sleep,duration, stress]])#membentuk array input
 
+    y_output = model.predict(input_data)
+    # if st.write(f"Prediksi nilai kepatuhan perawatan pasien dari model {model_option} adalah: {y_output[0]:.2f} %"):
+    if y_output[0] <= 30 :
+        st.error(f"Prediksi nilai kepatuhan perawatan pasien dari model {model_option} adalah: {y_output[0]:.2f} % ,termasuk pada Kepatuhan kurang.")
+    elif y_output[0] >=80:
+        st.success(f"Prediksi nilai kepatuhan perawatan pasien dari model {model_option} adalah: {y_output[0]:.2f} % , termasuk pada Kepatuhan baik")
+    else : 
+        st.warning(f"Prediksi nilai kepatuhan perawatan pasien dari model {model_option} adalah: {y_output[0]:.2f} %, termasuk pada kepatuhan sedang")
 
-#     prediction = model.predict(input_data) #melakukan prediksi dengan model
+    #     prediction = model.predict(input_data) #melakukan prediksi dengan model
 
-#     st.write(prediction)
+    #     st.write(prediction)
 
-#     if prediction[0] <= 30 :
-#         st.error("Kepatuhan kurang.")
-#     elif prediction[0] >=80:
-#         st.success("Kepatuhan baik")
-#     else : 
-#         st.warning("Kepatuhan sedang")
